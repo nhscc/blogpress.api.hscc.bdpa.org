8548c17f43b65d068633ba56b1b1df2ed44332d1
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2023-05-31 13:56:48 -0700

    feat(src): add authorizationHeaderToOwnerAttribute to backend/api exports

--- src/backend/api.ts
+++ src/backend/api.ts
@@ -1,5 +1,8 @@
 import { getEnv } from 'universe/backend/env';
+import { deriveSchemeAndToken, getAttributes } from 'multiverse/next-auth';
+
 import type { PageConfig } from 'next';
+import type { IncomingHttpHeaders } from 'node:http';
 
 /**
  * The default app-wide Next.js API configuration object.
@@ -15,3 +18,19 @@ export const defaultConfig: PageConfig = {
     }
   }
 };
+
+/**
+ * Returns the owner token attribute cross-referenced by the
+ * `authorizationHeader`.
+ */
+export async function authorizationHeaderToOwnerAttribute(
+  authorizationHeader: Required<IncomingHttpHeaders['authorization']>
+) {
+  const { owner } = await getAttributes({
+    target: await deriveSchemeAndToken({
+      authString: authorizationHeader
+    })
+  });
+
+  return owner;
+}
